30a0222524e034635dc4a6ee2718ff6c8f48dacb
raeesbb8 <league@Mac-mini-3.local>
2016-10-15 11:33:02 -0700

    10/15/16

---
+++ level0/Greeter.java
@@ -0,0 +1,10 @@
+package level0;
+
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+	String ans = JOptionPane.showInputDialog(null, "What's your favarite anime?");
+	JOptionPane.showMessageDialog(null, ans  is nice);
+}
+}
---
+++ level0/whileloopchallegen.java
@@ -0,0 +1,26 @@
+package level0;
+
+public class whileloopchallegen {
+public static void main(String[] args) {
+	int Num = 1;
+	while(Num<=4){
+		System.out.println(Num);
+		Num++;
+	}
+	int Num2 = -8;
+	while(Num2<=-4){
+		System.out.println(Num2);
+		Num2++;
+	}
+	int Num3 = 10;
+	while(Num3>=4){
+		System.out.println(Num3);
+		Num3=Num3-2;
+	}
+	int Num4 = 2;
+	while(Num4<=16){
+		System.out.println(Num4);
+		Num4=Num4+
+	}
+}
+}
