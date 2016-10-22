65661551a3267893f244a257a6740497466aebe3
laflaka416 <league@Mac-mini.local>
2016-10-22 11:34:14 -0700

    10/22/16

---
+++ level0/riddleMeThis.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+import jdk.nashorn.internal.scripts.JO;
+
+public class riddleMeThis {
+public static void main(String[] args) {
+	String name = JOptionPane.showInputDialog(null, " Is 2 + 2 = 5? ");
+	if( name.equals( "no" )){
+
+	}
+}
+}
