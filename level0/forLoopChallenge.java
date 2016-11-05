2c812316640319b541c4cda3730183df8ae9a06f
laflaka416 <league@Mac-mini-2.local>
2016-11-05 11:40:02 -0700

    11/5/16

---
+++ level0/forLoopChallenge.java
@@ -0,0 +1,9 @@
+package level0;
+
+public class forLoopChallenge {
+public static void main(String[] args) {
+	for(int num = 2; num<= 10; num =num+2){
+		System.out.println(num);
+	}
+}
+}
---
+++ level0/yearsAlive.java
@@ -0,0 +1,24 @@
+package level0;
+
+public class yearsAlive {
+	public static void main(String[] args) {
+		int Num = 0;
+		while (Num <= 14){
+			System.out.println(Num);
+			Num++;
+
+		}
+		
+		
+		for(int age = 0;age <=14;age++){
+			System.out.println(age);
+		}
+	
+		
+		
+		
+		
+		
+		}
+	}
+
